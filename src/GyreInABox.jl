813acecef8a1d673de7b8a0fdf19b7d2b5f4d714
Matt Graham <matthew.m.graham@gmail.com>
2025-12-02 18:13:11 +0000

    Factor out sigmoidal profile function

--- src/GyreInABox.jl
+++ src/GyreInABox.jl
@@ -497,6 +497,17 @@ function setup_model(
 end
 
 """
+Sigmoidal depth profile which smoothly varies from `f_top` at `z = 0` to `f_bottom` at
+`z = -depth` with length scale for smooth transition `ℓ` and offset for midpoint of
+smooth transition `z_0`.
+
+$(SIGNATURES)
+"""
+function sigmoidal_depth_profile(z, z_0, ℓ, f_bottom, f_top)
+    f_bottom + (f_top - f_bottom) * (1  + tanh((z - z_0) / ℓ)) / 2
+end
+
+"""
 Initial temperature at latitude `φ` and depth `z` with parameters `p`.
 
 $(SIGNATURES)
@@ -508,9 +519,8 @@ profile which smoothly varies from a latitude dependent surface temperature to a
 constant deep ocean temperature with a sigmoidal profile.
 """
 function initial_temperature(φ, z, p)
-    p.T_abyssal + (
-        reference_surface_temperature(φ, p) - p.T_abyssal
-    ) * (1 + tanh((z - p.z_thermocline) / p.ℓ_thermocline)) / 2
+    T_surface = reference_surface_temperature(φ, p)
+    sigmoidal_depth_profile(z, p.z_thermocline, p.ℓ_thermocline, p.T_abyssal, T_surface)
 end
 
 """
@@ -525,9 +535,8 @@ which smoothly varies from a latitude dependent surface salinity to a constant
 deep ocean salinity with a sigmoidal profile.
 """
 function initial_salinity(φ, z, p)
-    p.S_0 + (
-        reference_surface_salinity(φ, p) - p.S_0
-    ) * (1 + tanh((z - p.z_halocline) / p.ℓ_halocline)) / 2
+    S_surface = reference_surface_salinity(φ, p)
+    sigmoidal_depth_profile(z, p.z_halocline, p.ℓ_halocline, p.S_0, S_surface)
 end
 
 """
